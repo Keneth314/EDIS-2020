becf4a3f8c0dd5c7e53b97ed85759a08e115fdcf
Keneth Lopez <jkeneyth2904@gmail.com>
2020-11-13 12:40:01 -0500

    Busqueda algoritmos EDIS-S3

---
+++ busqueda.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // Algoritmo de búsqueda
+using namespace std;
+
+int array[7] = {12, 18, -20, 0, 15, 12, 9};
+int i, j, n;
+
+
+main(){
+	
+	int a;
+	a = 5/2;
+	cout << a;
+	
+}
